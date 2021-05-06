496273d0f359dbd47514d7035631da759f3d47c9
Евгений Юрин <evgenijurin@MacBook-Pro-Evgenij.local>
2021-05-07 00:23:07 +0200

    Second commit cykles

--- script.js
+++ script.js
@@ -8,13 +8,47 @@ const personalMovieDB = {
     privat: false 
 } ; 
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
 
-       personalMovieDB.movies[a] = b;
-       personalMovieDB.movies[c] = d;
+    for (let i =0 ; i < 2 ; i++) { 
+
+       const  a = prompt('Один из последних просмотренных фильмов?', ''),  
+              b = prompt('На сколько оцените его?', '');
+
+        if (a !=null && b != null && a !=' ' && b!= '' && a.length < 50) {
+        personalMovieDB.movies[a] = b ; 
+        console.log ('Done') ; 
+        }
+
+        else {
+            console.log ('Error') ;
+            i -- ; 
+        }
+
+    }
+
+
+
+
+    if (personalMovieDB.count < 10 ) { 
+        console.log ('Вы просмотерли мало фильмов') ; 
+    }
+
+    else if ( personalMovieDB.count >= 10 && personalMovieDB.count <30  ) { 
+console.log ('Вы среднячок!'); 
+    }
+    else if ( personalMovieDB.count >= 30 && personalMovieDB.count <50 ) {
+        console.log ('Вы киноман!'); 
+    }
+
+    else {
+        console.log ('Error') ; 
+    }
+
+
 
        console.log(personalMovieDB); 
